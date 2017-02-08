8d3d4acbb18d4ce1c4e59471a3c03426cd742895
Simonas Pauliukevičius <simonaspa@wix.com>
2017-02-08 16:18:46 +0200

    more game logic

--- iframe.js
+++ iframe.js
@@ -1,38 +1,80 @@
 var pi = document.body.appendChild(document.createElement('script'));
-pi.onload = function() {
-  
-   var app = new PIXI.Application(960, 600, {backgroundColor : 0x1099bb});
+pi.onload = function ()
+{
+    var app = new PIXI.Application(960, 600, { backgroundColor: 0x1099bb });
+    var renderer = app.renderer;
 
     // The application will create a canvas element for you that you 
     // can then insert into the DOM.
     document.body.appendChild(app.view);
 
-    // load the texture we need
-    PIXI.loader.add('bunny', 'https://varnius.github.io/kanaka/assets/musa.png').load(function(loader, resources) {
+    // Load things
 
-        // This creates a texture from a 'bunny.png' image.
-        var bunny = new PIXI.Sprite(resources.bunny.texture);
+    var assetLocation = !window.dev ? 'https://varnius.github.io/kanaka/' : '';
 
-        // Setup the position of the bunny
-        bunny.x = app.renderer.width / 2;
-        bunny.y = app.renderer.height / 2;
+    PIXI.loader.add('drill', assetLocation + 'assets/musa.png');
+    PIXI.loader.add('regularTile', assetLocation + 'assets/tile-test.png');
+    PIXI.loader.once('complete', onAssetsLoaded);
+    PIXI.loader.load();
 
-        // Rotate around the center
-        bunny.anchor.x = 0.5;
-        bunny.anchor.y = 0.5;
+    var bunny;
+    var stage = app.stage;
 
-        // Add the bunny to the scene we are building.
-        app.stage.addChild(bunny);
+    function onAssetsLoaded(loader, resources)
+    {
+        init();
 
-        // Listen for frame updates
-        app.ticker.add(function() {
-             // each frame we spin the bunny around a bit
+        function init() {
+            // This creates a texture from a 'bunny.png' image.
+            bunny = new PIXI.Sprite(resources.drill.texture);
+
+            // Setup the position of the bunny
+            bunny.x = app.renderer.width / 2;
+            bunny.y = app.renderer.height / 2;
+
+            // Rotate around the center
+            bunny.anchor.x = 0.5;
+            bunny.anchor.y = 0.5;
+
+            // Add the bunny to the scene we are building.
+            app.stage.addChild(bunny);
+
+            // Listen for frame updates
+            app.ticker.add(tick);
+            generateGrid();
+        }
+
+        // Render loop
+
+        function tick() {
             bunny.rotation += 0.01;
-        });
-    });
-}
-pi.src = 'https://varnius.github.io/kanaka/pixi.min.js';   
+        }
 
-console.log('window', window);
-console.log('this', this);
-console.log('debug top', window.top.document);
+        // Generate level
+
+        function generateGrid() {
+            var tile;
+            var container = new PIXI.Container();
+            stage.addChild(container);
+
+            var numTilesX = 15;
+            var numTilesY = 10;
+            var tileSize = Math.round(renderer.width / numTilesX);
+
+            for (var i = 0; i < numTilesX; i++) {
+                for (var j = 0; j < numTilesY; j++) {
+
+                    var texture = resources.regularTile.texture;
+
+                    tile = new PIXI.Sprite(texture);
+                    tile.position.x = i * tileSize;
+                    tile.position.y = j * tileSize;
+
+                    container.addChild(tile);
+                }
+            }
+        }
+    }
+};
+
+pi.src = 'https://varnius.github.io/kanaka/pixi.min.js';
